ed18b75c3f074ef6fc00c7dca1655871dfb1e46d
Mike Nardella Jr <1310994+nardellajr@users.noreply.github.com>
2024-01-01 15:36:16 -0500

    add comments about how the Time complexity is determined

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,6 +1,12 @@
-
 # create Quicksort algorithm
 
+
+# Not choosing the correct pivot point effects Time complexity
+# In the "height of the call stack" size
+# Best case for the call stack  O(log n) and worse case O(n)
+# We have to touch every element so O(n)
+# Know take O(n) * O(log n) = O(n log n)  -- Best
+# and       O(n) * O(n)     = O(n**2)     -- Worst
 def quicksort(arr):
     # base case, nothing to sort
     if len(arr) < 2:
