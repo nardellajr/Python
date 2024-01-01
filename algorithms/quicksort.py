494a4533caa514df69877962bd475e6ce03da926
Mike Nardella Jr <1310994+nardellajr@users.noreply.github.com>
2024-01-01 17:12:56 -0500

    add code to select random pivot

--- algorithms/quicksort.py
+++ algorithms/quicksort.py
@@ -1,22 +1,31 @@
 # create Quicksort algorithm
+import random
 
 
 # Not choosing the correct pivot point effects Time complexity
 # In the "height of the call stack" size
 # Best case for the call stack  O(log n) and worse case O(n)
-# We have to touch every element so O(n)
+# We have to touch every element when sorting so O(n)
 # Know take O(n) * O(log n) = O(n log n)  -- Best
 # and       O(n) * O(n)     = O(n**2)     -- Worst
+
+# Choosing a random element for the pivot, will get you O(log n)
 def quicksort(arr):
     # base case, nothing to sort
     if len(arr) < 2:
         return arr
     else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i < pivot]
-        greater = [i for i in arr[1:] if i > pivot]
+        # pivot = arr[0]
+        # less = [i for i in arr[1:] if i < pivot]
+        # greater = [i for i in arr[1:] if i > pivot]
 
-        return quicksort(less) + [pivot] + quicksort(greater)
+        pivot = random.choice(arr)
+        equal = [i for i in arr if i == pivot]
+        less = [i for i in arr if i < pivot]
+        greater = [i for i in arr if i > pivot]
+
+        # return quicksort(less) + [pivot] + quicksort(greater)
+        return quicksort(less) + equal + quicksort(greater)
 
 
 if __name__ == '__main__':
