61191764c801ba898b0eaa46dab022f5b570a95f
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2024-01-03 08:35:46 -0500

    misc files trying a couple of different things

---
+++ Longestsubstring.py
@@ -0,0 +1,22 @@
+
+
+"""A very similar to the first is the second mission of the series with only one distinction is that you should
+look in a completely different way. You need to find the first longest substring with all unique letters.
+For example, in substring "abca" we have two substrings with unique letters "abc" and "bca",
+but we should take the first one, so the answer is "abc".
+"""
+
+
+def non_repeat(line):
+
+    """How to create substring from line"""
+
+    return line
+
+
+if __name__ == "__main__":
+
+    assert non_repeat('aaaaa') == 'a', "First"
+    assert non_repeat('abdjwawk') == 'abdjw', "Second"
+    assert non_repeat('abcabcffab') == 'abcf', "Third"
+    print('"Run" is good. How is "Check"?')
---
+++ Test.py
@@ -0,0 +1,12 @@
+
+i = [1, 2, 3]
+print(len(i))
+
+print('not len(i) : ', not len(i))
+
+j = []
+print(len(j))
+
+if not len(j):
+    print("True")
+
